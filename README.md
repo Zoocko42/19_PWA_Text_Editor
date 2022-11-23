98edb7a9d69eb7946f7126f62000fa3e038d95a1
Zachariah Schneider <zoocko@gmail.com>
2022-11-22 22:25:33 -0500

    This update add plugins and loaders to the webpack.config.js folder

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 19_PWA_Text_Editor
 A text editor for taking notes and code snippets, functional with or without an internet connection.
+
+## Usage Instructions
+None, currently non-functional
+
+## Credits
+Thanks to the Workbox docs for the Service Worker and Manifest Plugins information, the webpack pwa manifest npm site for webpack info,
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -18,12 +19,37 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Webpack Plugin',
+      }),
+      new WorkboxPlugin.GenerateSW(),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'service-worker.js',
+      }), 
+      new WebpackPwaManifest({
+        name: 'PWA Text Editor',
+        short_name: 'PWATE',
+        description: 'A Progressive Web App for ',
+        background_color: '#ffffff',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+      })
     ],
 
     module: {
       rules: [
-        
+        { test: /\.css$/, use: 'css-loader' },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
       ],
     },
   };
